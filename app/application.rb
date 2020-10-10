eeeab88c8b953432ebfd897e35a414ee1a3b4c10
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-10 17:19:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
-    
+    (Time.now.to_i % 2).zero? ? ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
     
     resp.finish
     
