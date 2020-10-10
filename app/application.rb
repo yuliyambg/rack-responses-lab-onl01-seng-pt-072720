21a3530de5653a2b57785c08e18b29d3f9ee53b5
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-10 17:26:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     
     
-    if Time.now.to_i ==(0..12)
+    if (Time.now.to_i) ==(0..12)
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
